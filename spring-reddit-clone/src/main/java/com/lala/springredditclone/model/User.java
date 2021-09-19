cbc29de6143b22c3a831a63a8a9873918b53a942
lalit ghongade <lalitghongade1451999@gmail.com>
2021-09-19 13:54:27 +0530

    added all models

--- spring-reddit-clone/src/main/java/com/lala/springredditclone/model/User.java
+++ spring-reddit-clone/src/main/java/com/lala/springredditclone/model/User.java
@@ -1,24 +1,36 @@
 package com.lala.springredditclone.model;
 
 
-import javax.persistence.*;
+import static javax.persistence.GenerationType.SEQUENCE;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-
 public class User {
-	@Id
-	@GeneratedValue(strategy = SEQUENCE)
-	private Long userId;
-	
-
+    @Id
+    @GeneratedValue(strategy = SEQUENCE)
+    private Long userId;
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Email
+    @NotEmpty(message = "Email is required")
+    private String email;
+    private Instant created;
+    private boolean enabled;
 }
